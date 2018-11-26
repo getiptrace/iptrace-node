f843eefd6542e8a5a04783df24242e99c0972f07
Muhamad Omar <mohamadomr33@gmail.com>
2018-11-26 22:27:47 +0200

    [update] add a pre build and pre publish npm commands

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-const greet = "hello";
-
-console.log(greet);
+const defaultAwesomeFunction = (name) => {
+    const returnStr = `I am the Default Awesome Function, fellow comrade! - ${name}`;
+    return returnStr;
+  };
+  
+  const awesomeFunction = () => 'I am just an Awesome Function';
+  
+  export default defaultAwesomeFunction;
+  
+  export { awesomeFunction };
